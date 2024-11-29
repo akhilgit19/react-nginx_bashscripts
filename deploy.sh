8ad5c19fc0f13c26149c5b0f052c2c3066e5f825
akhilgit19 <138980055+akhilgit19@users.noreply.github.com>
2024-11-29 13:43:23 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 pods_id=$(microk8s kubectl get pods -n react-nginx-namespace |grep react |awk {'print $1'})
 microk8s kubectl delete pod $pods_id -n react-nginx-namespace
+cd /home/ubuntu/react-chart/
+helm upgrade react-chart .
